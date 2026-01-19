3a581c5d4fb25504c9c1060b4300010fe39c1806
Александр Волков <AVolkov211@yandex.ru>
2026-01-19 19:47:19 +0300

    Второе задание после lint

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,24 @@
 function checkingSringLength(string, number) {
-  return (string.length <= number) ? console.log("Условие выполняется ТЕРН ОП") : console.log("Условие не выполняется, строка больше указанной длинны ТЕРН ОП");
+  return (string.length <= number) ? console.log('Условие выполняется ТЕРН ОП') : console.log('Условие не выполняется, строка больше указанной длинны ТЕРН ОП');
 }
 
-checkingSringLength("Четыре", 6);
+checkingSringLength('Четыре', 6);
 
 function CheckPalindrome(line) {
-let result = line.replaceAll(' ', '');
-let register = result.toLowerCase();
-let emptyLine = "";
+  const result = line.replaceAll(' ', '');
+  const register = result.toLowerCase();
+  let emptyLine = '';
 
-for (let i = register.length-1; i >= 0; i--) {
-  emptyLine += register[i];
-  console.log(emptyLine);
-}
+  for (let i = register.length - 1; i >= 0; i--) {
+    emptyLine += register[i];
+    console.log(emptyLine);
+  }
 
-if (register === emptyLine) {
-  console.log("Это палиндром");
-} else {
-  console.log(register + " - Это НЕ палиндром!!!");
-}
-}
-
-CheckPalindrome("Лёша на полке клопа нашёл");
-
-function extractNumbers(line) {
-let toLine = line.toString().replaceAll(' ', '');
-let emptyLine = "";
-
-for (let i = 0; i <= toLine.length - 1; i++) {
-  let result = parseInt(toLine[i]);
-  console.log(result + " это значение переменной result");
-
-  if (result !== NaN) {
-    emptyLine += toLine[i];
-    console.log("Это из цикла  сейчас переменная emptyLine = " + parseInt(emptyLine));
+  if (register === emptyLine) {
+    console.log('Это палиндром');
+  } else {
+    console.log(register + ' - Это НЕ палиндром!!!');
   }
 }
-}
 
-extractNumbers("2");
+CheckPalindrome('Лёша на полке клопа нашёл');
